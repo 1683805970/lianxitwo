63ed80707466583ff376f4bf217ff83681ab8a8b
1683805970 <1683805970@qq.com>
2022-06-29 07:05:12 +0800

    bay one

---
+++ 03-webpack的loader/src/js/aaa.js
@@ -0,0 +1,4 @@
+export const name = 'lei'
+export const age = 80
+export const heider = 1.80
+  //  --config ./webpack.config.js
---
+++ 03-webpack的loader/src/main.js
@@ -0,0 +1,4 @@
+import {name,age,heider} from "./js/aaa"
+console.log(name);
+console.log(age);
+console.log(heider);
